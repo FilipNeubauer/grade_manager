e119ff7a95c76ceafe895c65b1e4c054fad12dce
Filip Neubauer <filipneub@seznam.cz>
2021-10-04 12:56:37 +0200

    add_grade function

--- grades_manager.py
+++ grades_manager.py
@@ -1,4 +1,5 @@
 import sqlite3
+from datetime import date
 
 conn = sqlite3.connect("My_Grades.db")
 c = conn.cursor()
@@ -13,3 +14,21 @@ def add_subject():
             break
         except:
             print("[This subject already exists or you enter invalid name]")
+
+
+def add_grade():
+    while True:
+        subject_name = input("[Enter the name of the Subject]: ")
+        c.execute("SELECT count(name) FROM sqlite_master WHERE type='table' AND name='{}'".format(subject_name))
+        if c.fetchone()[0] == 1:
+            grade = input("[Enter the grade]: ")
+            try:
+                float(grade)
+                today = date.today()
+                d = today.strftime("%d/%m/%Y")
+                c.execute("INSERT INTO {} VALUES ({}, {})".format(subject_name, grade, d))
+                break
+            except:
+                print("[Invalid grade]")
+                break
+        print("[Invalid Subject]")
